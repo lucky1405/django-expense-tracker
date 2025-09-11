5b3afa03394cff98f56bc82e8dcb78b31f82613e
lucky1405 <162043891+lucky1405@users.noreply.github.com>
2025-09-11 20:33:23 +0530

    set login page as site index

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,10 +3,12 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'), 
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    
     path('categories/', views.catergory_list, name='category_list'),
     path('add/', views.add_transaction, name='add_transaction'),
     path('update/<int:pk>/', views.update_transaction, name='update_transaction'),
